65db9b947ceedc9f1fc9c18f6476007ccfa54c22
sami-eurus <sami.ul.islam@eurustechnologies.com>
2023-04-10 13:21:46 +0500

    added v2 text

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                         PHP CRUD With Datatables Using Twitter Bootstrap
+                         PHP CRUD With Datatables Using Twitter Bootstrap V2
                         </h1>
                        
                     </div>
